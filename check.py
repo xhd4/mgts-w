c5dae65f6b83accdbb427195ad9f39b823ef8afb
xhda <example.gmail.com>
2017-07-01 21:11:22 +0300

    Refact_sec

--- check.py
+++ check.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 import time
 import signal
 import argparse
-
 from random import randrange
 from multiprocessing import Process
 
@@ -12,7 +13,9 @@ from scapy.layers.dot11 import Dot11Beacon
 
 from core.packet import PacketHandler
 
+
 class ScapyScanner(PacketHandler):
+
     def __init__(self, scan_int, power, connect_int):
         super(ScapyScanner, self).__init__(power, connect_int)
         self.process_channel_hop = Process(target=self._channel_hopper)
@@ -50,12 +53,17 @@ def main():
     parser.add_argument('-power', type=int, help='limit by power access point signal ')
     parser.add_argument('-con_int', help='interface for connect an access point')
     apt = parser.parse_args()
+
     object_scanner = ScapyScanner(apt.scan_int, apt.power, apt.con_int)
     try:
-        print
-        object_scanner.run()
-    except Exception:
-        pass
+        if apt.scan is not None and apt.con_int is not None:
+            try:
+                object_scanner.run()
+            except Exception:
+                pass
+    except AttributeError as error:
+        print error
+
 
 if __name__ == '__main__':
     main()
